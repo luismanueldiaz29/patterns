2d9b343fc36756815bc4931b68ab97f55d92cfde
Oscar Morales <odmorales@unicesar.edu.co>
2023-07-04 13:04:59 -0500

    FlyweightPattern

---
+++ PatternFlyWeight/PatternFlyWeight/Classes/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternFlyWeight.Classes
+{
+    public class Car
+    {
+        public string Owner { get; set; } = string.Empty;
+        public string Number { get; set; } = string.Empty;
+        public string Company { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+    }
+}
---
+++ PatternFlyWeight/PatternFlyWeight/Classes/FlyWeight.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace PatternFlyWeight.Classes
+{
+    public class FlyWeight
+    {
+        private Car _sharedState;
+
+        public FlyWeight(Car car)
+        {
+            this._sharedState = car;
+        }
+
+        public void Operation(Car uniqueState)
+        {
+            string s = JsonConvert.SerializeObject(this._sharedState);
+            string u = JsonConvert.SerializeObject(uniqueState);
+            Console.WriteLine($"Flyweight: Displaying shared {s} and unique {u} state.");
+        }
+    }
+}
